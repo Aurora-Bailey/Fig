1aec1e9d6504525c5e615ad55f1032a49225dce6
Brad Thompson <bradleyglind@gmail.com>
2016-09-09 14:34:59 -0600

    add sass support

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -21,8 +21,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1 {
-  color: #42b983;
-}
+<style lang="sass" scoped>
+h1
+  color: #42b983
+
 </style>
