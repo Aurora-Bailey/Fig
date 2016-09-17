049bbe9f8fb31273f21da10e33a94d582ee740c1
Brad Thompson <bradleyglind@gmail.com>
2016-09-17 03:47:39 -0600

    More comments

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../dist/index.html'),//'../dist_android/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),//'../dist_android'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',//'/android_asset/www/',
     productionSourceMap: true,
