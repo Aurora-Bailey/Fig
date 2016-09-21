20c9ce08213105b349f36cd177093c302ebc496e
Brad Thompson <bradleyglind@gmail.com>
2016-09-21 10:51:46 -0600

    basic websocket server

--- websocket/Worker.js
+++ websocket/Worker.js
@@ -9,80 +9,70 @@ var http = require('http'),
   app = express(),
   workerPort = 8000 + 777,
   process = false,
-  WORKER_INDEX = false;
+  WORKER_INDEX = false,
+  NODE_ENV = false;
 
-module.exports.setup = function(p){
+
+
+module.exports.setup = function (p) {
   process = p;
   WORKER_INDEX = process.env.WORKER_INDEX;
-
+  NODE_ENV = process.env.WORKER_INDEX;
   console.log('I\'m worker ' + WORKER_INDEX);
 
-  process.send('READY!!');
-};
+  process.on('message', function (m, c) {
+    //Later
+  });
 
+  wss.on('connection', function connection(ws) {
+    ws.connected = true;
+    ws.sendObj = function (obj) {
+      try {
+        ws.send(JSON.stringify(obj));
+      } catch (err) {
+        if (NODE_ENV == 'development')console.log(err);
+      }
+    };
+    ws.on('message', function incoming(data) {
+      try {
+        var d = JSON.parse(data);
+        console.log(d);
+      }
+      catch (err) {
+        console.log('Bad Packet: ', data, err);
+      }
+    });
+
+    ws.on('close', function () {
+      ws.connected = false;
+    });
+
+    ws.sendObj({m: 'hi'});
+  });
+
+  app.use(function (req, res) {// This is sent when the WebSocket is requested as a web page
+    try {
+      res.send('WebSocket -_- ' + WORKER_INDEX);
+    } catch (err) {
+      console.log('express res.send');
+      console.log(err);
+    }
+  });
+
+  server.on('request', app);
+  server.listen(workerPort, function () {
+    console.log('Worker ' + WORKER_INDEX + ' listening on port ' + server.address().port)
+  });
+
+  process.send('worker ' + WORKER_INDEX + ' READY!!');
+};
 
 
 /*
 
 
- app.use(function (req, res) {
- // This is sent when the WebSocket is requested as a webpage
- try {
- res.send('WebSocket');
- } catch (err) {
- console.log('express res.send');
- console.log(err);
- }
- });
 
- wss.on('connection', function connection(ws) {
- ws.connected = true;
- ws.sendObj = function (obj) {
- try {
- ws.send(JSON.stringify(obj));
- } catch (err) {
- if (NODE_ENV == 'development')console.log(err);
- }
- };
- ws.on('message', function incoming(data) {
- try {
- var d = JSON.parse(data);
 
- }
- catch (err) {
- console.log('Bad Packet: ', data, err);
- }
- });
 
- ws.on('close', function () {
- ws.connected = false;
- });
-
- ws.sendObj({m: 'hi'});
- });
-
- server.on('request', app);
- server.listen(gamePort, function () {
- console.log('NODE_ENV: ' + NODE_ENV + ' Server: ' + SERVER_NAME + ' Room: "' + ROOM_NAME + '" Worker: ' + WORKER_INDEX + ' Port: ' + server.address().port)
- });
-
- // Get message from master and check if need pass to http server
- process.on('message', function (m, c) {
- if ("doit" === m) {
- try {
- server.emit('connection', c);
- c.resume();
- } catch (err) {
- console.log('err: doit recieve');
- console.log(err);
- }
- } else if (typeof m === 'object') {
- if (m.m == 'population') {
- Game.numConnectedAllRooms = m.p;
- Game.capacityAllRooms = m.v;
- }
-
- }
- });
 
  */
